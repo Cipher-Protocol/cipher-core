9992ec3e6222a8dac6474f9161add4bf87e7396f
Ray Huang <ray880301@gmail.com>
2023-09-18 23:46:49 +0800

    update contract

--- contracts/UtxoStorage.sol
+++ contracts/UtxoStorage.sol
@@ -4,21 +4,24 @@ pragma solidity ^0.8.20;
 import {IncrementalTreeData} from "@zk-kit/incremental-merkle-tree.sol/IncrementalBinaryTree.sol";
 import {IVerifier} from "./interfaces/IVerifier.sol";
 
+struct TreeData {
+    IncrementalTreeData incrementalTreeData;
+    mapping(uint256 => bool) nullifiers;
+}
+
 abstract contract UtxoStorage {
+    type Currency is address;
+
     address internal constant DEFAULT_ETH_ADDRESS = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
 
     uint256 internal constant SNARK_SCALAR_FIELD =
         21888242871839275222246405745257275088548364400416034343698204186575808495617;
 
-    IVerifier internal immutable _verifier;
+    IVerifier internal immutable verifier;
 
-    uint32 internal _totalLeaves;
-
-    IncrementalTreeData internal _treeData;
-
-    mapping(uint256 => bool) internal _nullifiers;
+    mapping(Currency => TreeData) internal treeData;
 
     constructor(address verifierAddr) {
-        _verifier = IVerifier(verifierAddr);
+        verifier = IVerifier(verifierAddr);
     }
 }
