2d1caeba93a7b50286e401a0c0f9241fed5917f9
Ray Huang <ray880301@gmail.com>
2023-10-14 15:17:30 +0800

    add comments

--- contracts/interfaces/ICipher.sol
+++ contracts/interfaces/ICipher.sol
@@ -1,4 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-interface ICipher {}
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ICipherVerifier} from "./ICipherVerifier.sol";
+import {Proof, PublicInfo, RelayerInfo} from "../utils/DataType.sol";
+
+interface ICipher {
+    function initTokenTree(IERC20 token) external;
+
+    function cipherTransact(Proof calldata proof, PublicInfo calldata publicInfo) external payable;
+
+    function cipherTransactWithRelayer(
+        Proof calldata proof,
+        PublicInfo calldata publicInfo,
+        RelayerInfo calldata relayerInfo
+    ) external payable;
+
+    function registerAsRelayer(string memory relayerMetadataUri) external;
+
+    function updateRelayerMetadataUri(string memory newRelayerMetadataUri) external;
+
+    function getTreeDepth(IERC20 token) external view returns (uint256);
+
+    function getTreeRoot(IERC20 token) external view returns (uint256);
+
+    function getTreeLeafNum(IERC20 token) external view returns (uint256);
+
+    function getTreeZeroes(IERC20 token, uint256 level) external view returns (uint256);
+
+    function getTreeLastSubtrees(IERC20 token, uint256 level) external view returns (uint256[2] memory);
+
+    function getRelayerMetadataUri(address relayerAddr) external view returns (string memory);
+
+    function getVerifier() external view returns (ICipherVerifier);
+
+    function isNullified(IERC20 token, uint256 nullifier) external view returns (bool);
+
+    function isValidRoot(IERC20 token, uint256 root) external view returns (bool);
+}
--- contracts/libraries/TreeLib.sol
+++ contracts/libraries/TreeLib.sol
@@ -7,24 +7,39 @@ import {Constants} from "./Constants.sol";
 import {Errors} from "./Errors.sol";
 import {Events} from "./Events.sol";
 
+/** ***** ***** ***** ***** ***** ***** ***** ***** ***** *****
+        Tree data structure in storage
+***** ***** ***** ***** ***** ***** ***** ***** ***** *****  */
 struct TreeData {
+    /// @notice The incremental merkle tree data
     IncrementalTreeData incrementalTreeData;
-    /// @notice The index of the latest root in history roots
+    /// @notice The index of the latest history root in history roots array
     uint8 historyRootsIdx;
-    /// @notice History roots (The 32 valid roots before the latest root)
-    /// @notice To avoid others updating the root when user calculating the proof in client side
+    /// @notice The history roots array (The 32 valid roots before the latest root)
+    ///         to avoid others updating the root when user calculating the proof in client side
     uint256[32] historyRoots;
+    /// @notice The nullifiers mapping to avoid double spend
     mapping(uint256 => bool) nullifiers;
 }
 
 library TreeLib {
     using IncrementalBinaryTree for IncrementalTreeData;
 
+    /// @notice Update the history roots array and index
+    /// @dev Add the current root to the history roots array before insert new commitments
+    /// @param _tree The tree data
+    /// @param _root The new root
     function updateHistoryRoot(TreeData storage _tree, uint256 _root) internal {
         _tree.historyRoots[_tree.historyRootsIdx] = _root;
         _tree.historyRootsIdx = uint8(addmod(_tree.historyRootsIdx, 1, Constants.VALID_HISTORY_ROOTS_SIZE));
     }
 
+    /// @notice Update the nullifiers mapping
+    /// @dev Set the nullifiers to true to avoid double spend
+    /// @param _tree The tree data
+    /// @param _token The token of the token tree
+    /// @param _nullifiers The nullifiers array
+    /// @param nullifierLen The length of nullifiers array
     function updateNullifiers(
         TreeData storage _tree,
         IERC20 _token,
@@ -40,6 +55,12 @@ library TreeLib {
         }
     }
 
+    /// @notice Insert commitments to the incremental merkle tree
+    /// @dev Insert commitments to the incremental merkle tree and update the tree root
+    /// @param _tree The tree data
+    /// @param _token The token of the token tree
+    /// @param _commitments The commitments array
+    /// @param commitmentLen The length of commitments array
     function insertCommitments(
         TreeData storage _tree,
         IERC20 _token,
@@ -53,6 +74,12 @@ library TreeLib {
         }
     }
 
+    /// @notice Check if the root is in the history roots array
+    /// @dev Check from the latest history root to the oldest history root to save gas
+    ///      i.e. If `historyRootsIdx` = 27, check from `historyRoots`[27] -> [26] -> [25] ... [1] -> [0] -> [31] ... [29] -> [28]
+    /// @param _tree The tree data
+    /// @param _root The root to check
+    /// @return isHistoryRoot True if the root is in the history roots array
     function isHistoryRoot(TreeData storage _tree, uint256 _root) internal view returns (bool) {
         // ex: 32 - 27 = 5
         uint256 start = Constants.VALID_HISTORY_ROOTS_SIZE - _tree.historyRootsIdx;
@@ -68,10 +95,19 @@ library TreeLib {
         return false;
     }
 
+    /// @notice Check if the root is the latest root
+    /// @param _tree The tree data
+    /// @param _root The root to check
+    /// @return isLastestRoot True if the root is the latest root
     function isLastestRoot(TreeData storage _tree, uint256 _root) internal view returns (bool) {
         return _root == _tree.incrementalTreeData.root;
     }
 
+    /// @notice Check if the root is valid
+    /// @dev Check if the root is the latest root or in the history roots array
+    /// @param _tree The tree data
+    /// @param _root The root to check
+    /// @return isValidRoot True if the root is valid
     function isValidRoot(TreeData storage _tree, uint256 _root) internal view returns (bool) {
         return isLastestRoot(_tree, _root) || isHistoryRoot(_tree, _root);
     }
