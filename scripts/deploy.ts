a7eb85853eb1dc92cab3a22343390d3ff758ed6d
Ray Huang <ray880301@gmail.com>
2023-10-16 20:59:33 +0800

    update deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,11 +2,12 @@ import {
   Cipher,
   CipherVerifier__factory,
   Cipher__factory,
-  IncrementalBinaryTree,
 } from "@/typechain-types";
 import { getString } from "@/utils/helper";
 import { Wallet } from "ethers";
 import { ethers } from "hardhat";
+const circomlibjs = require("circomlibjs");
+const { createCode, generateABI } = circomlibjs.poseidonContract;
 
 export const main = async () => {
   const provider = new ethers.providers.JsonRpcProvider(
@@ -32,37 +33,24 @@ export const main = async () => {
 
   // Deploy PoseidonT3 contract
   console.log("Deploying PoseidonT3 contract...");
-  const PoseidonT3 = await ethers.getContractFactory("PoseidonT3");
-  const poseidonT3 = await PoseidonT3.deploy();
+  const poseidonT3Factory = new ethers.ContractFactory(
+    generateABI(2),
+    createCode(2),
+    deployer
+  );
+  const poseidonT3 = await poseidonT3Factory.deploy();
   await poseidonT3.deployed();
   console.log("PoseidonT3 deployed to:", poseidonT3.address);
 
-  // Deploy IncrementalBinaryTreeFactory contract
-  console.log("Deploying IncrementalBinaryTreeFactory contract...");
-  const IncrementalBinaryTreeFactory = await ethers.getContractFactory(
-    "IncrementalBinaryTree",
-    {
-      libraries: {
-        PoseidonT3: poseidonT3.address,
-      },
-    }
-  );
-  const incrementalBinaryTree =
-    (await IncrementalBinaryTreeFactory.deploy()) as IncrementalBinaryTree;
-  await incrementalBinaryTree.deployed();
-  console.log(
-    "IncrementalBinaryTreeFactory deployed to:",
-    incrementalBinaryTree.address
-  );
-
   // Deploy Cipher contract
   console.log("Deploying Cipher contract...");
-  const cipherFactory = (await ethers.getContractFactory("Cipher", {
-    libraries: {
-      IncrementalBinaryTree: incrementalBinaryTree.address,
-    },
-  })) as Cipher__factory;
-  const cipher = (await cipherFactory.deploy(cipherVerifier.address)) as Cipher;
+  const cipherFactory = (await ethers.getContractFactory(
+    "Cipher"
+  )) as Cipher__factory;
+  const cipher = (await cipherFactory.deploy(
+    cipherVerifier.address,
+    poseidonT3.address
+  )) as Cipher;
   await cipher.deployed();
   console.log("Cipher deployed to:", cipher.address);
 };
