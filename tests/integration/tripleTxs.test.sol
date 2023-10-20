6be4f9ca2457a4066b413d9d854e2ef35b219953
wiasliaw <wiasliaw@protonmail.com>
2023-10-20 16:02:21 +0800

    chore: update pragam

--- tests/integration/tripleTxs.test.sol
+++ tests/integration/tripleTxs.test.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.19;
 
 import {BaseTest} from "../Base_Test.sol";
 import {TestDataFile} from "../utils/loadTestdata.sol";
